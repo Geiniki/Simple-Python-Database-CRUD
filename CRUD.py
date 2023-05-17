2c661f17117fea33d08c2fce05a80900449258f6
OsamaBinRamen <101690013+Geiniki@users.noreply.github.com>
2023-05-17 23:58:16 +0800

    Update CRUD.py

--- CRUD.py
+++ CRUD.py
@@ -1,6 +1,7 @@
 #Comments are Filipino(Tagalog) Based. Feel free to Google Translate. 
 #Made by Mark Dennis Concha and Jay-r Donesa.
 #Under The Supervision of Mr. Manuel Sentillas
+#REMOVE ALL BITMAP OR REPLACE THE ICO FILE TO AVOID RUNTIME ERRORS!!!!
 
 import pymysql
 from tkinter import *
@@ -27,12 +28,28 @@ def refreshTable():
     for array in read():
         my_tree.insert(parent='', index='end', iid=array, text="", values=(array), tag="orow")
 
-    my_tree.tag_configure('orow', background='#EEEEEE', font=('Arial', 12))
+    my_tree.tag_configure('orow', background='#EEEEEE', font=('Berlin Sans FB', 12))
     my_tree.grid(row=8, column=0, columnspan=5, rowspan=11, padx=10, pady=20)
 
 root = Tk()
-root.title("Work Immersion CRUD")
-root.geometry("1080x720")
+root.title("STEM PTECH IMMERSION PROJECT")
+root.config(bg=('#48426D'))
+root.iconbitmap(r'Icon.ico')
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+
+window_width = 1050
+window_height = 700
+
+
+x = (screen_width // 2) - (window_width // 2)
+y = (screen_height // 2) - (window_height // 2)
+
+# Set ng Window geometry
+root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
 
 my_tree = ttk.Treeview(root)
 
@@ -75,6 +92,8 @@ def add():
 
     if (studid == "" or studid == " ") or (studname == "" or studname == " ") or (age == "" or age == " ") or (department == "" or department == " ") or (professor == "" or professor == " "):
         messagebox.showinfo("Error","Please fill up the blank entry")
+        root.iconbitmap(r'Icon.ico')
+           
         return
     else:
         try:
@@ -85,30 +104,29 @@ def add():
             conn.close()
         except:
             messagebox.showinfo("Error", "Stud ID already exist")
+            root.iconbitmap(r'Icon.ico')
             return
 
     refreshTable()
     
-# Reset Button na Function
-def reset():
-    decision = messagebox.askquestion("Warning!!", "Delete all data?")
-    if decision != "yes":
-        return 
+def clear():
+    # Check if any entry is empty
+    if not studidEntry.get() or not studnameEntry.get() or not ageEntry.get() or not departmentEntry.get() or not professorEntry.get():
+        # Display an error message box
+        messagebox.showerror("Error", "There is nothing to Clear 🤔 ")
     else:
-        try:
-            conn = connection()
-            cursor = conn.cursor()
-            cursor.execute("DELETE FROM students")
-            conn.commit()
-            conn.close()
-        except:
-            messagebox.showinfo("Error", "Sorry an error occured")
-            return
-
-        refreshTable()
+        # Clear all entries
+        studidEntry.delete(0, 'end')
+        studnameEntry.delete(0, 'end')
+        ageEntry.delete(0, 'end')
+        departmentEntry.delete(0, 'end')
+        professorEntry.delete(0, 'end')
+ 
+  
 # Delete Button na Function
 def delete():
     decision = messagebox.askquestion("Warning!!", "Delete the selected data?")
+    root.iconbitmap(r'Icon.ico')
     if decision != "yes":
         return 
     else:
@@ -122,94 +140,19 @@ def delete():
             conn.close()
         except:
             messagebox.showinfo("Error", "Sorry an error occured")
+            root.iconbitmap(r'Icon.ico')
             return
 
         refreshTable()
-#Select Button na Function
-def select():
-    try:
-        selected_item = my_tree.selection()[0]
-        studid = str(my_tree.item(selected_item)['values'][0])
-        studname = str(my_tree.item(selected_item)['values'][1])
-        age = str(my_tree.item(selected_item)['values'][2])
-        department = str(my_tree.item(selected_item)['values'][3])
-        professor = str(my_tree.item(selected_item)['values'][4])
-
-        setph(studid,1)
-        setph(studname,2)
-        setph(age,3)
-        setph(department,4)
-        setph(professor,5)
-    except:
-        messagebox.showinfo("Error", "Please select a data row")
-
-    conn = connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM students WHERE STUDID='"+
-    studid+"' or studname='"+
-    studname+"' or age='"+
-    age+"' or department='"+
-    department+"' or professor='"+
-    professor+"' ")
-    
-    try:
-        result = cursor.fetchall()
-
-        for num in range(0,5):
-            setph(result[0][num],(num+1))
-
-        conn.commit()
-        conn.close()
-    except:
-        messagebox.showinfo("Error", "No data found")
-
-# Update Button na Function
-def update():
-    selectedStudid = ""
-
-    try:
-        selected_item = my_tree.selection()[0]
-        selectedStudid = str(my_tree.item(selected_item)['values'][0])
-    except:
-        messagebox.showinfo("Error", "Please select a data row")
-
-    studid = str(studidEntry.get())
-    studname = str(studnameEntry.get())
-    age = str(ageEntry.get())
-    department = str(departmentEntry.get())
-    professor = str(professorEntry.get())
-
-    if (studid == "" or studid == " ") or (studname == "" or studname == " ") or (age == "" or age == " ") or (department == "" or department == " ") or (professor == "" or professor == " "):
-        messagebox.showinfo("Error", "Please fill up the blank entry")
-        return
-    else:
-        try:
-            conn = connection()
-            cursor = conn.cursor()
-            cursor.execute("UPDATE students SET STUDID='"+
-            studid+"', studname='"+
-            studname+"', age='"+
-            age+"', department='"+
-            department+"', professor='"+
-            professor+"' WHERE STUDID='"+
-            selectedStudid+"' ")
-            conn.commit()
-            conn.close()
-        except:
-            messagebox.showinfo("Error", "Stud ID already exist")
-            return
-
-    refreshTable()
-
 # Pag Declare at Style ng mg Values
-label = Label(root, text="STEM PTECH 12B STUDENT CRUD", font=('Arial Bold', 30))
+label = Label(root, text="STUDENT CRUD", font=('Berlin Sans FB', 28),bg=('#48426D'),fg=('#F0C38E'))
 label.grid(row=0, column=0, columnspan=8, rowspan=2, padx=50, pady=40)
 
-studidLabel = Label(root, text="Student ID", font=('Arial', 15))
-studnameLabel = Label(root, text="Sudent Name", font=('Arial', 15))
-ageLabel = Label(root, text="Age", font=('Arial', 15))
-departmentLabel = Label(root, text="Department", font=('Arial', 15))
-professorLabel = Label(root, text="Professor", font=('Arial', 15))
+studidLabel = Label(root, text="Student ID:", font=('Berlin Sans FB Bold', 15),bg=('#48426D'),fg=('#F0C38E'))
+studnameLabel = Label(root, text="Sudent Name:", font=('Berlin Sans FB Bold', 15),bg=('#48426D'),fg=('#F0C38E'))
+ageLabel = Label(root, text="Student Age:", font=('Berlin Sans FB Bold', 15),bg=('#48426D'),fg=('#F0C38E'))
+departmentLabel = Label(root, text="Student Department:", font=('Berlin Sans FB Bold', 15),bg=('#48426D'),fg=('#F0C38E'))
+professorLabel = Label(root, text="Student Professor:", font=('Berlin Sans FB Bold', 15),bg=('#48426D'),fg=('#F0C38E'))
 
 studidLabel.grid(row=3, column=0, columnspan=1, padx=75, pady=5)
 studnameLabel.grid(row=4, column=0, columnspan=1, padx=50, pady=5)
@@ -217,11 +160,11 @@ ageLabel.grid(row=5, column=0, columnspan=1, padx=50, pady=5)
 departmentLabel.grid(row=6, column=0, columnspan=1, padx=50, pady=5)
 professorLabel.grid(row=7, column=0, columnspan=1, padx=50, pady=5)
 
-studidEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph1)
-studnameEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph2)
-ageEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph3)
-departmentEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph4)
-professorEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph5)
+studidEntry = Entry(root, width=45, bd=3, font=('Berlin Sans FB', 15),bg=('#312C51'),fg=('#F0C38E'), textvariable = ph1)
+studnameEntry = Entry(root, width=45, bd=3, font=('Berlin Sans FB', 15),bg=('#312C51'),fg=('#F0C38E'), textvariable = ph2)
+ageEntry = Entry(root, width=45, bd=3, font=('Berlin Sans FB', 15),bg=('#312C51'),fg=('#F0C38E'), textvariable = ph3)
+departmentEntry = Entry(root, width=45, bd=3, font=('Berlin Sans FB', 15),bg=('#312C51'),fg=('#F0C38E'), textvariable = ph4)
+professorEntry = Entry(root, width=45, bd=3, font=('Berlin Sans FB', 15),bg=('#312C51'),fg=('#F0C38E'), textvariable = ph5)
 
 studidEntry.grid(row=3, column=1, columnspan=4, padx=5, pady=0)
 studnameEntry.grid(row=4, column=1, columnspan=4, padx=5, pady=0)
@@ -231,39 +174,31 @@ professorEntry.grid(row=7, column=1, columnspan=4, padx=5, pady=0)
 
 addBtn = Button(
     root, text="Add", padx=65, pady=25, width=1, 
-    bd=5, font=('Arial', 15), bg="#ed9898", command=add)
-updateBtn = Button(
-    root, text="Update", padx=65, pady=25, width=1,
-    bd=5, font=('Arial', 15), bg="#ed9898", command=update)
+    bd=10, font=('Berlin Sans FB', 15),bg=('#F0C38E'),fg=('#48426D'), command=add)
 deleteBtn = Button(
     root, text="Delete", padx=65, pady=25, width=1,
-    bd=5, font=('Arial', 15), bg="#ed9898", command=delete)
-resetBtn = Button(
-    root, text="Reset", padx=65, pady=25, width=1,
-    bd=5, font=('Arial', 15), bg="#ed9898", command=reset)
-selectBtn = Button(
-    root, text="Select", padx=65, pady=25, width=1,
-    bd=5, font=('Arial', 15), bg="#ed9898", command=select)
+    bd=10, font=('Berlin Sans FB', 15),bg=('#F0C38E'),fg=('#48426D'), command=delete)
+clearBtn = Button(
+    root, text="Clear", padx=65, pady=25, width=1, 
+    bd=10, font=('Berlin Sans FB', 15),bg=('#F0C38E'),fg=('#48426D'), command=clear)
 
 addBtn.grid(row=3, column=5, columnspan=1, rowspan=2)
-updateBtn.grid(row=5, column=5, columnspan=1, rowspan=2)
-deleteBtn.grid(row=7, column=5, columnspan=1, rowspan=2)
-resetBtn.grid(row=9, column=5, columnspan=1, rowspan=2)
-selectBtn.grid(row=11, column=5, columnspan=1, rowspan=2)
+deleteBtn.grid(row=5, column=5, columnspan=1, rowspan=2)
+clearBtn.grid(row=7, column=5, columnspan=1, rowspan=2)
 
 #ttk.Style Para sa Color ng Tree
 style = ttk.Style()
 style.theme_use('clam')
-style.configure("Treeview.Heading", font=('Arial Bold', 15))
-style.map('Treeview',background=[('selected','pink')])
+style.configure("Treeview.Heading", font=('Berlin Sans FB Bold', 15),background='#F0C38E',foreground='#48426D')
+style.map('Treeview',background=[('selected','sandy brown')])
 my_tree['columns'] = ("StudentID","StudentName","Age","Department","Professor")
 
 my_tree.column("#0", width=0, stretch=NO)
 my_tree.column("StudentID", anchor=W, width=170)
-my_tree.column("StudentName", anchor=W, width=150)
-my_tree.column("Age", anchor=W, width=150)
-my_tree.column("Department", anchor=W, width=165)
-my_tree.column("Professor", anchor=W, width=150)
+my_tree.column("StudentName", anchor=W, width=170)
+my_tree.column("Age", anchor=W, width=170)
+my_tree.column("Department", anchor=W, width=170)
+my_tree.column("Professor", anchor=W, width=170)
 
 my_tree.heading("StudentID", text="Student ID", anchor=W)
 my_tree.heading("StudentName", text="Student Name", anchor=W)
